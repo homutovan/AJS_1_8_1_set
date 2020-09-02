0af663f0e7db8d86e50227401064f3fbb50695a4
Andrey Khomutov <homutovan@live.com>
2020-09-02 23:19:14 +0700

    add Team class and tests

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# js env starter
+[![Build status](https://ci.appveyor.com/api/projects/status/jbins646j397tc17/branch/master?svg=true)](https://ci.appveyor.com/project/homutovan/ajs-1-8-1-set/branch/master)
 
-[![Build status](https://ci.appveyor.com/api/projects/status/pgje30jtk2oevnyy/branch/master?svg=true)](https://ci.appveyor.com/project/homutovan/starter/branch/master)
+# Домашнее задание к лекции «Занятие 8. "Контейнеры»
+
+## Set
 
 ### Installation
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,19 @@
-console.log('app worked');
+export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(character) {
+    const { size } = this.members;
+    this.members.add(character);
+    if (size === this.members.size) throw new Error('This character is already in the team!');
+  }
+
+  addAll(characterList) {
+    this.members = new Set([...this.members, ...characterList]);
+  }
+
+  toArray() {
+    return [...this.members];
+  }
+}
